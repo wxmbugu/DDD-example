700ca69e482beb53fd0cd182d08a7b85f98ec1b3
Stephen Wambugu <wambugumacharia35@gmail.com>
2023-03-24 00:16:21 +0300

    update tasks.go
    secrets

--- internal/api/tasks.go
+++ internal/api/tasks.go
@@ -13,7 +13,7 @@ type SendEmails struct {
 }
 
 func NewSenderMail() SendEmails {
-	mail := mailer.NewMailer(25, "wambugumacharia35@gmail.com", "sandbox.smtp.mailtrap.io", "11b93bd1c2b240", "ad888e5474a880") //TODO:Don't send this to upstream with credentials
+	mail := mailer.NewMailer(25, "", "", "", "") //TODO:Don't send this to upstream with credentials
 	return SendEmails{
 		mailer: mail,
 	}
